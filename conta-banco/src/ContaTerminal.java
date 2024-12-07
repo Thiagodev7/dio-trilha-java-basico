5c33e85382d4cf6f7b7ea539d485f2568240e629
Thiago <email@thiagols.com.br>
2024-12-07 08:33:04 -0300

    finalizando o segundo teste

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,23 +2,28 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
-        System.out.println("Por favor, digite o número da Agência !");
-        String agencia = scanner.next();
-
-        System.out.println("Por favor, digite o número da conta !");
-        int numero = scanner.nextInt();
-
-        System.out.println("Por favor, digite o nome do responsavel da conta !");
-        String nomeCliente = scanner.next();
-
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Nome: " + nomeCliente);
-        System.out.println("Agencia: " + agencia );
-        System.out.println("Conta " + numero );
-
-    }
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+      int N = leitor.nextInt();
+      String jogador1, jogador2;
+// TODO: complete os espaços em branco com sua solução para o problema
+      for (int i = 0; i < N; i++) {
+          jogador1 = leitor.next();
+          jogador2 = leitor.next();
+          if (jogador1.equalsIgnoreCase("ataque") && jogador2.equalsIgnoreCase("ataque")) System.out.println("Aniquilacao mutua");
+          else if (jogador1.equalsIgnoreCase("ataque") && jogador2.equalsIgnoreCase("papel")) System.out.println("Jogador 1 venceu");
+          else if (jogador1.equalsIgnoreCase("ataque") && jogador2.equalsIgnoreCase("pedra")) System.out.println("Jogador 1 venceu");
+          
+          else if (jogador1.equalsIgnoreCase("papel") && jogador2.equalsIgnoreCase("ataque")) System.out.println("Jogador 2 venceu");
+          else if (jogador1.equalsIgnoreCase("papel") && jogador2.equalsIgnoreCase("papel")) System.out.println("Ambos venceram");
+          else if (jogador1.equalsIgnoreCase("papel") && jogador2.equalsIgnoreCase("pedra")) System.out.println("Jogador 2 venceu");
+          
+          else if (jogador1.equalsIgnoreCase("pedra") && jogador2.equalsIgnoreCase("ataque")) System.out.println("Jogador 2 venceu");
+          else if (jogador1.equalsIgnoreCase("pedra") && jogador2.equalsIgnoreCase("papel")) System.out.println("Jogador 1 venceu");
+          else if (jogador1.equalsIgnoreCase("pedra") && jogador2.equalsIgnoreCase("pedra")) System.out.println("Sem ganhador");
+          
+          
+          else System.out.println( "erro" );
+      }
+  }
 }
