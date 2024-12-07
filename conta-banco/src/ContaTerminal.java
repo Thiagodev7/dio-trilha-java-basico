6589ca722b057b2dcffa6bac50933505940b9d26
Thiago <email@thiagols.com.br>
2024-12-07 07:03:09 -0300

    concluido o primeiro teste

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta !");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do responsavel da conta !");
+        String nomeCliente = scanner.next();
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Nome: " + nomeCliente);
+        System.out.println("Agencia: " + agencia );
+        System.out.println("Conta " + numero );
+
     }
 }
